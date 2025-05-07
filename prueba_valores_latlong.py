1c39bd0644aa842d9250035e501cbc1fd53b41eb
agstnrdz <agstnrodriguez@gmail.com>
2025-05-07 14:34:11 -0300

    update-py

--- prueba_valores_latlong.py
+++ prueba_valores_latlong.py
@@ -1,17 +1,20 @@
+## Conversión de valores planos a coordenadas decimales
+
 import pandas as pd
 
-# Leer el Excel y cargar lat y long como string
-df = pd.read_excel('archivo_latlong.xlsx', dtype={'lat': str, 'long': str})
+# .xlsx
+df = pd.read_excel('archivo_latlong.xlsx')
 
-# Función para insertar el punto decimal después del tercer carácter
+# Función insertar el punto decimal después del tercer carácter
 def insertar_punto(valor):
-    return float(valor[:3] + '.' + valor[3:])
+    valor_str = str(int(valor))  # convertimos a entero y luego a string
+    return float(valor_str[:3] + '.' + valor_str[3:])
 
-# Aplicamos la función a las columnas lat y long
+# Aplico la función a las columnas lat y long
 df['lat_decimal'] = df['lat'].apply(insertar_punto)
 df['long_decimal'] = df['long'].apply(insertar_punto)
 
-# Guardamos el DataFrame modificado en un nuevo archivo
+# Exporto el GDF modificado en un nuevo archivo
 df.to_excel('coordenadas_decimales.xlsx', index=False)
 
 print("Conversión terminada.")
