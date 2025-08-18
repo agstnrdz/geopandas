bdc11cc2779b61bb77e48e693be718e26f713659
agstnrdz <agstnrodriguez@gmail.com>
2025-08-18 10:02:32 -0300

    update python file

--- separador_coordenadas.py
+++ separador_coordenadas.py
@@ -1,7 +1,11 @@
-import pandas as pd
+# Cargar librería
+import pandas as py
 
+# Leer el archivo previamente cargado en carpeta
 df = pd.read_excel("nombre_archivo.xlsx")
 
+# Dividir y crear columnas
 df[['Lat', 'Lng']] = df['Coordenadas'].str.strip().str.split(',', expand = True)
 
+# Guardar
 df.to_excel("nombre_archivo.xlsx", index = False)
