6b7797e82b173cc06d55a91af579daf860725dd3
agstnrdz <agstnrodriguez@gmail.com>
2025-09-23 10:33:36 -0300

    Test

--- separador_coordenadas.py
+++
@@ -1,7 +0,0 @@
-import pandas as import pd
-
-df = pd.read_excel("archivo.xlsx")
-
-df[['Lat', 'Lng']] = df['Coordenadas'].str.strip().str.split(',', expand=True)
-
-df.to_excel("archivo.xlsx", index = False)
